6feabcbb8539b626486ab4d739e581602fc9a541
koshtijatin26 <koshtijatin26@gmail.com>
2026-01-24 09:17:03 +0530

    general

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,14 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('clear', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('config:clear');
+    Artisan::call('config:cache');
+    Artisan::call('view:clear');
+    Artisan::call('storage:link');
+    return "Cleared!";
+});
 Route::get('/', function () {
     return view('welcome');
 });
