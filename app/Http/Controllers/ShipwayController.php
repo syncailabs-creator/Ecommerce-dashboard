a3e9f608233e8544149b8396d1cdd969b1f6a9c8
koshtijatin26 <koshtijatin26@gmail.com>
2026-01-26 22:01:42 +0530

    general

--- app/Http/Controllers/ShipwayController.php
+++ app/Http/Controllers/ShipwayController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Http;
+
+use App\Models\ShipwayOrder;
+use App\Models\ShipwayOrderStatus;
 
 class ShipwayController extends Controller
 {
@@ -11,9 +15,40 @@ class ShipwayController extends Controller
     {
         Log::info('Shipway Webhook Received:', $request->all());
 
+        $orderId = $request->input('order_id');
+        
+        $order = ShipwayOrder::where('order_id', $orderId)->first();
+
+        if ($order) {
+            Log::info("Order found in Shipway orders: " . $orderId);
+            
+            $orderStatus = $request->input('new_current_status');
+
+            $existingStatus = ShipwayOrderStatus::where('shipway_order_id', $orderId)
+                ->where('status', $orderStatus)
+                ->first();
+
+            if ($existingStatus) {
+               
+                $existingStatus->update([
+                    'updated_datetime' =>  $request->input('status_time'),
+                    'updated_at' => now()
+                ]);
+            } else {                
+                ShipwayOrderStatus::create([
+                    'shipway_order_id' => $orderId,
+                    'status' => $orderStatus,
+                    'datetime' =>  $request->input('status_time'),
+                    'updated_datetime' =>  $request->input('status_time'),
+                ]);
+            }
+            
+        } else {
+            Log::info("Order not found in Shipway orders: " . $orderId);          
+        }
+
         return response()->json([
-            'status' => 'success',
-            'data' => $request->all()
+            'status' => 'success'
         ]);
     }
 }
